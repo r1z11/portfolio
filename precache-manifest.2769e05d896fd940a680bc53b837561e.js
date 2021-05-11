0e0f471c3104e54f711fa41a2b4a9c8fddb0b0e7
Eric Sentongo <eric_sentongo@yahoo.com>
2021-05-11 18:02:56 +0300

    Updates

---
+++ precache-manifest.2769e05d896fd940a680bc53b837561e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7b07d81d5c673cf773f9fd36304707",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e7cac388eb4f6a2b5167",
+    "url": "/portfolio/static/css/main.6fe4d705.chunk.css"
+  },
+  {
+    "revision": "8fa518b2dae5a1be457d",
+    "url": "/portfolio/static/js/2.12c9f761.chunk.js"
+  },
+  {
+    "revision": "e7cac388eb4f6a2b5167",
+    "url": "/portfolio/static/js/main.3ec87e9b.chunk.js"
+  },
+  {
+    "revision": "5e7224d1e371a77c2384",
+    "url": "/portfolio/static/js/runtime-main.0d1e8c07.js"
+  },
+  {
+    "revision": "639252876d3630bca93cdc061de464e8",
+    "url": "/portfolio/static/media/apps.63925287.png"
+  },
+  {
+    "revision": "68dd64ffabbf12b06e69cb433da92722",
+    "url": "/portfolio/static/media/apricot-guest-house.68dd64ff.png"
+  },
+  {
+    "revision": "36efa426a225f27b1e22f329ad7e9b3a",
+    "url": "/portfolio/static/media/breakthrough-miracle-life.36efa426.png"
+  },
+  {
+    "revision": "c6d9fb761b8739f4d53b88ccb3771dc4",
+    "url": "/portfolio/static/media/gardenia-spa.c6d9fb76.png"
+  },
+  {
+    "revision": "c0ce27fe3f7f658512bc8561720afa22",
+    "url": "/portfolio/static/media/hair-goals-extensions.c0ce27fe.png"
+  },
+  {
+    "revision": "897ac372a9c5a5b95000cd5683d8681f",
+    "url": "/portfolio/static/media/moving-ads-uganda.897ac372.png"
+  },
+  {
+    "revision": "4dbd4e41f89dc93e418e87d4042b2da3",
+    "url": "/portfolio/static/media/restful-apis.4dbd4e41.png"
+  },
+  {
+    "revision": "e17064d12e228c5b0254f15cba140a13",
+    "url": "/portfolio/static/media/rotafield-schools.e17064d1.png"
+  },
+  {
+    "revision": "1760252cc3384dbc4c286c5688f899ad",
+    "url": "/portfolio/static/media/rotaryeclubug.1760252c.png"
+  },
+  {
+    "revision": "a126b0361cdcf3d95a903da5ad56bcba",
+    "url": "/portfolio/static/media/seo.a126b036.png"
+  },
+  {
+    "revision": "00319d708091e591b66c28f3b99627de",
+    "url": "/portfolio/static/media/ug-mix-maestro-2.00319d70.png"
+  },
+  {
+    "revision": "d71389c985bc8dcac43903de0c5df444",
+    "url": "/portfolio/static/media/ugagri.d71389c9.png"
+  },
+  {
+    "revision": "f9636c451c12cbbd53a6cf621baffffa",
+    "url": "/portfolio/static/media/web-apps.f9636c45.png"
+  },
+  {
+    "revision": "b63b7b453c653e7a43f9f8701ea94ec4",
+    "url": "/portfolio/static/media/web-hosting.b63b7b45.png"
+  },
+  {
+    "revision": "859ad31327fcf8a4f67e20c1c1eae95e",
+    "url": "/portfolio/static/media/websites.859ad313.png"
+  }
+]);
