492edbb4b35811c1e24f891501b48d1fa777d03c
eric <eric_sentongo@yahoo.com>
2024-04-06 16:05:19 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ebdf363e1fbb54020c61450879b16596.js"
+  "/portfolio/precache-manifest.577cbb782d18755b019644ad1c62f37d.js"
 );
 
 self.addEventListener('message', (event) => {
