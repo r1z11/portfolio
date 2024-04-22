b57d774b24c492af7f8135d7b01089d011f2efe6
eric <eric_sentongo@yahoo.com>
2024-04-22 10:21:31 +0300

    projects component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+
 import { services, projects } from '../constants/data';
+import Projects from './Projects';
 
 const Content = () => {
 
     const [key, setKey] = useState('all');
 
-    const sequence = [0,3,4,7,8,11,12,15];
+    const sequence = [0, 3, 4, 7, 8, 11, 12, 15];
 
     return (
         <div>
@@ -50,7 +52,7 @@ const Content = () => {
                     <div className="row">
                         {
                             projects.map((item, index) => (
-                                <div key={index} className="col-md-6 my-3 pl-3">
+                                <div key={'all-' + index} className="col-md-6 my-3 pl-3">
                                     <div className={(sequence.indexOf(index) !== -1) ? "bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden" : "bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden"}>
                                         <div className="my-3 py-3">
                                             <h2 className="display-5">{item.name}</h2>
@@ -68,22 +70,7 @@ const Content = () => {
                 <Tab eventKey="websites" title="Websites">
 
                     <div className="row">
-                        {
-                            projects.map((item, index) => (
-                                <>
-                                    {item.category === 'website' ?
-                                        <div key={index} className="col-md-6 my-3 pl-3">
-                                            <div className={(sequence.indexOf(index) !== -1) ? "bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden" : "bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden"}>
-                                                <div className="my-3 py-3">
-                                                    <h2 className="display-5">{item.name}</h2>
-                                                    <p className="lead">{item.description}</p>
-                                                </div>
-                                                <a href={item.link} target="_blank" rel="noopener noreferrer"><img className="shadow-lg" style={styles.box} src={item.image} alt={item.alt} /></a>
-                                            </div>
-                                        </div> : null}
-                                </>
-                            ))
-                        }
+                        <Projects projects={projects} category='website' sequence={sequence} />
                     </div>
                 </Tab>
 
@@ -91,44 +78,14 @@ const Content = () => {
                 <Tab eventKey="apps" title="Apps">
 
                     <div className="row">
-                        {
-                            projects.map((item, index) => (
-                                <>
-                                    {item.category === 'mobile-app' ?
-                                        <div key={index} className="col-md-6 my-3 pl-3">
-                                            <div className={(sequence.indexOf(index) !== -1) ? "bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden" : "bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden"}>
-                                                <div className="my-3 py-3">
-                                                    <h2 className="display-5">{item.name}</h2>
-                                                    <p className="lead">{item.description}</p>
-                                                </div>
-                                                <a href={item.link} target="_blank" rel="noopener noreferrer"><img className="shadow-lg" style={styles.box} src={item.image} alt={item.alt} /></a>
-                                            </div>
-                                        </div> : null}
-                                </>
-                            ))
-                        }
+                        <Projects projects={projects} category='mobile-app' sequence={sequence} />
                     </div>
                 </Tab>
 
                 {/* Web Apps */}
                 <Tab eventKey="web-apps" title="Web Apps">
                     <div className="row">
-                        {
-                            projects.map((item, index) => (
-                                <>
-                                    {item.category === 'web-app' ?
-                                        <div key={index} className="col-md-6 my-3 pl-3">
-                                            <div className={(sequence.indexOf(index) !== -1) ? "bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden" : "bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden"}>
-                                                <div className="my-3 py-3">
-                                                    <h2 className="display-5">{item.name}</h2>
-                                                    <p className="lead">{item.description}</p>
-                                                </div>
-                                                <a href={item.link} target="_blank" rel="noopener noreferrer"><img className="shadow-lg" style={styles.box} src={item.image} alt={item.alt} /></a>
-                                            </div>
-                                        </div> : null}
-                                </>
-                            ))
-                        }
+                        <Projects projects={projects} category='web-app' sequence={sequence} />
                     </div>
                 </Tab>
 
@@ -136,22 +93,7 @@ const Content = () => {
                 <Tab eventKey="wp-plugins" title="WordPress Plugins">
 
                     <div className="row">
-                        {
-                            projects.map((item, index) => (
-                                <>
-                                    {item.category === 'wp-plugin' ?
-                                        <div key={index} className="col-md-6 my-3 pl-3">
-                                            <div className={(sequence.indexOf(index) !== -1) ? "bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden" : "bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden"}>
-                                                <div className="my-3 py-3">
-                                                    <h2 className="display-5">{item.name}</h2>
-                                                    <p className="lead">{item.description}</p>
-                                                </div>
-                                                <a href={item.link} target="_blank" rel="noopener noreferrer"><img className="shadow-lg" style={styles.box} src={item.image} alt={item.alt} /></a>
-                                            </div>
-                                        </div> : null}
-                                </>
-                            ))
-                        }
+                        <Projects projects={projects} category='wp-plugin' sequence={sequence} />
                     </div>
                 </Tab>
             </Tabs>
@@ -173,9 +115,6 @@ const styles = {
         width: '100%',
         borderTopLeftRadius: 21,
         borderTopRightRadius: 21,
-    },
-    boxApp: {
-        marginTop: 38
     }
 }
 
